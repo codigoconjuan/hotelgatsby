d7debffaeb30e1bb0f265fadfe9b1adc2adc668b
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-19 17:04:00 -0600

    agregando una imagen

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from '@emotion/styled';
+
+const Nav = styled.nav`
+    display: flex;
+    justify-content: center;
+    padding-bottom: 3rem;
+    @media (min-width: 768px) {
+        padding-bottom: 0;
+    }
+`; 
+
+const NavLink = styled(Link)`
+    color: #FFFFFF;
+    font-size: 1.6rem;
+    font-weight: 700;
+    line-height: 1rem;
+    font-family: 'PT Sans', sans-serif;
+    text-decoration: none;
+    padding: 1rem;
+    margin-right: 1rem;
+
+    &:last-of-type {
+        margin-right: 0;
+    }
+    &.pagina-actual {
+        border-bottom: 2px solid #FFF;
+    }
+`;
 
 const Navegacion = () => {
     return ( 
-        <nav>
-            <Link to={'/'}>Inicio</Link>
-            <Link to={'/nosotros'}>Nosotros</Link>
-        </nav>
+        <Nav>
+            <NavLink 
+                to={'/'}
+                activeClassName="pagina-actual"
+            >Inicio</NavLink>
+            <NavLink 
+                to={'/nosotros'}
+                activeClassName="pagina-actual"
+            >Nosotros</NavLink>
+        </Nav>
      );
 }
  
