5b91abdf6e52fcd06246bde86b5d9c7693225b6b
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-18 19:22:10 -0600

    agregar hojas de estilos con helmet

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import Header from './header'
+import Header from './header';
+import Helmet from 'react-helmet';
 
 const Layout = (props) => {
     return ( 
         <>
+            <Helmet>
+                <title>Gatsby Hotel</title>
+                <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" />
+                <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700|Roboto:400,700&display=swap" rel="stylesheet" />
+            </Helmet>
+
             <Header />
             {props.children}
         </>
