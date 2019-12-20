7d7bde0e9958fc96ca19a2b37eda792448e8ae46
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-20 12:56:38 -0600

    creando paginas con gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,24 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+exports.createPages = async ({ actions, graphql, reporter }) => {
+    const resultado = await graphql(`
+        query {
+            allDatoCmsHabitacion {
+                nodes {
+                    slug
+                }
+            }
+        }
+    `);
 
-// You can delete this file if you're not using it
+    // console.log(resultado.data.allDatoCmsHabitacion.nodes);
+
+    if(resultado.errors) {
+        reporter.panic('No hubo resultados ', resultado.errors);
+    }
+
+    // Si hay paginas, crear los archivos
+    const habitaciones = resultado.data.allDatoCmsHabitacion.nodes;
+
+    
+
+    
+}
