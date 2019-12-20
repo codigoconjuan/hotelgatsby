85317f7d0d91c142cd013f7363ddeb7f01f9c37e
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-19 18:07:16 -0600

    agregando la imagen de fondo

--- src/components/imagenHotel.js
+++ src/components/imagenHotel.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
+import BackgroundImage from 'gatsby-background-image';
+import styled from '@emotion/styled';
+
+const ImageBackground = styled(BackgroundImage)`
+    height: 700px;
+`
 
 const ImagenHotel = () => {
 
@@ -8,17 +14,19 @@ const ImagenHotel = () => {
         image: file(relativePath: { eq:"8.jpg" } ) {
           sharp:childImageSharp {
             fluid {
-              srcSetWebp
+              ...GatsbyImageSharpFluid_withWebp
             }
           }
         }
       }
     `);
 
-    console.log(image);
+    // console.log(image.sharp.fluid);
 
     return ( 
-        <h1>Imagen aqui</h1>
+        <ImageBackground tag="section" fluid={image.sharp.fluid} fadeIn="soft" >
+
+        </ImageBackground>
     );
 }
  
