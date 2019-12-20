c99ad317b0c37740e9455843979f420a8bce2da9
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-19 20:02:48 -0600

    css al contenido de inicio

--- src/components/contenidoInicio.js
+++ src/components/contenidoInicio.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import Image from 'gatsby-image';
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
+
+const TextoInicio = styled.div`
+    padding-top: 4rem;
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+
+    @media (min-width: 768px) {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr) ;
+        column-gap: 2rem;
+    }
+
+    p {
+        line-height: 2;
+        
+    }
+`;
 
 const ContenidoInicio = () => {
 
@@ -26,12 +46,18 @@ const ContenidoInicio = () => {
 
     return ( 
         <>
-            <h2>{titulo}</h2>
+            <h2
+                css={css`
+                    text-align: center;
+                    font-size: 4rem;
+                    margin-top: 4rem;
+                `}
+            >{titulo}</h2>
 
-            <div>
+            <TextoInicio>
                 <p>{contenido}</p>
                 <Image fluid={imagen.fluid} />
-            </div>
+            </TextoInicio>
 
         </>
      );
