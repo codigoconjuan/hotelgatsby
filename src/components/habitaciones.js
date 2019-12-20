66e359c521c674f0ad04c08f22b71a679dcf61c8
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-20 13:12:13 -0600

    leyendo el contenido de las habitaciones

--- src/components/habitaciones.js
+++ src/components/habitaciones.js
@@ -1,6 +1,26 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-const HabitacionTemplate = () => {
+export const query = graphql`
+    query($slug: String!) {
+        allDatoCmsHabitacion(filter: { slug: { eq: $slug } }) {
+            nodes {
+                    titulo
+                    contenido
+                    imagen {
+                    fluid(maxWidth:1200) {
+                        ...GatsbyDatoCmsFluid
+                    }
+                }
+            }
+        }
+    }
+`
+
+const HabitacionTemplate = ({data}) => {
+
+    console.log(data);
+    
     return ( <h1>Habitacion.js</h1> );
 }
  
