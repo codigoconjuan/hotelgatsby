a84fb044d9b980158508d2adf03333c8ca41e92e
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-20 11:56:02 -0600

    finalizando nosotros

--- src/components/contenidoNosotros.js
+++ src/components/contenidoNosotros.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import Image from 'gatsby-image';
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
+
+const Contenido = styled.main`
+    padding-top: 4rem;
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+
+    @media (min-width: 768px) {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        column-gap: 3rem;
+    }
+    p {
+        line-height: 2;
+        margin-top: 2rem;
+    }
+`;
 
 const ContenidoNosotros = () => {
 
@@ -24,13 +43,19 @@ const ContenidoNosotros = () => {
 
     return ( 
         <>
-            <h2>{titulo}</h2>
-            <div>
+            <h2
+                css={css`
+                    margin-top: 4rem;
+                    text-align: center;
+                    font-size: 4rem;
+                `}
+            >{titulo}</h2>
+            <Contenido>
                 <p> {contenido} </p>
                 <Image 
                     fluid={imagen.fluid}
                 />
-            </div>
+            </Contenido>
 
         </>
      );
